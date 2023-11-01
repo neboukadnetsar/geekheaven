5b16f6d32e168fb8482f4aa2dbe7aa25eddb34ab
Gabriel D'Amour <A22.gabriel.damour@cegepsherbrooke.qc.ca>
2023-11-01 16:41:42 -0400

    remove const dbConfig

--- queries/DBPool.js
+++ queries/DBPool.js
@@ -1,6 +1,6 @@
 const Pool = require('pg').Pool;
 
-const dbConfig = require('../db_config');
+// const dbConfig = require('../db_config');
 
 module.exports = new Pool({
     connectionString: process.env.DBConfigLink,
