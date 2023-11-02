9893459ce062795df3cb540de6c41147353ec02f
Gabriel D'Amour <A22.gabriel.damour@cegepsherbrooke.qc.ca>
2023-11-02 06:19:44 -0400

    test ordre

--- app.js
+++ app.js
@@ -23,15 +23,14 @@ app.use(cookieParser());
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
 app.use('/api/produits', routesProduits);
 app.use('/api/utilisateur', routesUtilisateur);
 app.use('/api/adresse', routesAdresse);
 app.use('/api/commande', routesCommande);
 app.use('/api/panier', routesPanier);
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 class BasicStrategyModified extends BasicStrategy {
   constructor(options, verify) {
